9eb36084061d8e03293618e9e4e8f8918a40db7f
jcvdg <jcgriendt@gmail.com>
2022-06-02 22:38:48 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();  // allows app to use everything that express has to offer
+const cors = require('cors');
+const PORT = 8000;
+
+app.use(cors());
+
+let rappers = {
+    '21 savage': {
+        'age': 29,
+        'birthName': 'Sheyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England',
+    },
+    'chance the rapper': {
+        'age': 29,
+        'birthName': 'Chancelor Bennett',
+        'birthLocation': 'Chicago, Illinois',
+    },
+    'unknown': {
+        'age': 0,
+        'birthName': 'unknown',
+        'birthLocation': 'unknown',
+    }
+}
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + 'index.html');
+});
+
+app.get('/api/:tree', (req, res) => {
+    const rapperName = req.params.tree.toLowerCase();
+
+    if( rappers[rapperName] ) {
+        res.json(rappers[rapperName])
+    }else {
+        res.json(rappers['unknown'])
+    }
+    
+});
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`The server is now running on port ${PORT}!  Betta Go Catch It!`)
+});
